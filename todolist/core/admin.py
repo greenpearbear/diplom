3df5a74771c2dfb68fb7f84351788110371c4b21
greenpearbear <romadmitr12@gmail.com>
2022-07-14 22:27:32 +0700

    Настройка Django Admin

--- todolist/core/admin.py
+++ todolist/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    exclude = ['password']
+    search_fields = ('email', 'first_name', 'last_name', 'username')
+    readonly_fields = ('last_login', 'date_joined')
