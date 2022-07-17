b631c0c9eaf8c9c8c0e089dbd1fa0db1b0883409
greenpearbear <romadmitr12@gmail.com>
2022-07-17 17:03:31 +0700

    Собрал docker-compose.

---
+++ core/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    search_fields = ('email', 'first_name', 'last_name', 'username')
+    readonly_fields = ('last_login', 'date_joined')
