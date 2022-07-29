b768a68253c5ef24174c57599fb575f2f732287e
greenpearbear <romadmitr12@gmail.com>
2022-07-29 16:24:48 +0700

    Начало работы над досками.
    Модель + настройки миграции для уже созданных моделей.

--- goals/models.py
+++ goals/models.py
@@ -18,11 +18,23 @@ class DateModelMixin(models.Model):
         return super().save(*args, **kwargs)
 
 
+class Board(DateModelMixin):
+    class Meta:
+        verbose_name = "Доска"
+        verbose_name_plural = "Доски"
+
+    title = models.CharField(verbose_name="Название", max_length=255)
+    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+
+
 class GoalCategory(DateModelMixin):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
+    board = models.ForeignKey(
+        Board, verbose_name="Доска", on_delete=models.PROTECT, related_name="categories", null=True
+    )
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
@@ -66,3 +78,31 @@ class GoalComment(DateModelMixin):
     goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(verbose_name='Текст')
+
+
+class BoardParticipant(DateModelMixin):
+    class Meta:
+        unique_together = ("board", "user")
+        verbose_name = "Участник"
+        verbose_name_plural = "Участники"
+
+    class Role(models.IntegerChoices):
+        owner = 1, "Владелец"
+        writer = 2, "Редактор"
+        reader = 3, "Читатель"
+
+    board = models.ForeignKey(
+        Board,
+        verbose_name="Доска",
+        on_delete=models.PROTECT,
+        related_name="participants",
+    )
+    user = models.ForeignKey(
+        User,
+        verbose_name="Пользователь",
+        on_delete=models.PROTECT,
+        related_name="participants",
+    )
+    role = models.PositiveSmallIntegerField(
+        verbose_name="Роль", choices=Role.choices, default=Role.owner
+    )
