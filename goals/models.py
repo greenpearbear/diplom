7ba49f68b7c0c5b8564c8f3f6a992f3e5b547f6d
greenpearbear <romadmitr12@gmail.com>
2022-07-29 16:35:40 +0700

    Начало работы над досками.
    Модель + настройки миграции для уже созданных моделей.

--- goals/models.py
+++ goals/models.py
@@ -33,7 +33,7 @@ class GoalCategory(DateModelMixin):
         verbose_name_plural = "Категории"
 
     board = models.ForeignKey(
-        Board, verbose_name="Доска", on_delete=models.PROTECT, related_name="categories", null=True
+        Board, verbose_name="Доска", on_delete=models.PROTECT, related_name="categories"
     )
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
