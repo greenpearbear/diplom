9c8c859895881312f38c504bab26e7f5ea6899c5
greenpearbear <romadmitr12@gmail.com>
2022-08-03 12:04:26 +0700

    Правки goals.urls

--- goals/urls.py
+++ goals/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path("goal/create", goals.GoalCreateView.as_view()),
     path("goal/list", goals.GoalListView.as_view()),
     path("goal/<pk>", goals.GoalView.as_view()),
-    path("goal/goal_comment/create", comments.CommentsCreateView.as_view()),
-    path("goal/goal_comment/list", comments.CommentsListView.as_view()),
-    path("goal/goal_comment/<pk>", comments.CommentsView.as_view()),
-    path("goal/board/create", board.BoardCreateView.as_view()),
-    path("goal/board/list", board.BoardListView.as_view()),
-    path("goal/board/<pk>", board.BoardView.as_view()),
+    path("goal_comment/create", comments.CommentsCreateView.as_view()),
+    path("goal_comment/list", comments.CommentsListView.as_view()),
+    path("goal_comment/<pk>", comments.CommentsView.as_view()),
+    path("board/create", board.BoardCreateView.as_view()),
+    path("board/list", board.BoardListView.as_view()),
+    path("board/<pk>", board.BoardView.as_view()),
 
 ]
