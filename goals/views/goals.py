17b2aae9cad96af6b14c48dc6a43f2986c6864e5
greenpearbear <romadmitr12@gmail.com>
2022-08-04 17:54:23 +0700

    Правки классов доступа для вью.
    Добавление бота для телеграммы.

--- goals/views/goals.py
+++ goals/views/goals.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import permissions, filters
+from rest_framework import filters
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import LimitOffsetPagination
 
