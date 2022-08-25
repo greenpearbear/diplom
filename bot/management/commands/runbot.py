39ab784d44834d516b3ab82d6ed3632f02e9938b
greenpearbear <romadmitr12@gmail.com>
2022-08-25 21:29:55 +0700

    Исправил бота v2.1(create )

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -43,6 +43,7 @@ class Command(BaseCommand):
                 data.append(item.title)
             if "/cancel" in categories_response:
                 self.tg_client.send_message(msg.chat.id, 'Отмена создания цели')
+                return
             elif categories_response in data:
                 self.tg_client.send_message(msg.chat.id, "Введите заголовок цели")
                 response_goal = self.tg_client.get_updates(offset=self.offset + 1, timeout=60)
@@ -50,13 +51,17 @@ class Command(BaseCommand):
                     goal_response = item.message.text
                 if "/cancel" in goal_response:
                     self.tg_client.send_message(msg.chat.id, 'Отмена создания цели')
+                    return
                 else:
                     self.tg_client.send_message(msg.chat.id,
                                                 f"Категория - {categories_response} Цель - {goal_response}")
+                    return
             else:
                 self.tg_client.send_message(msg.chat.id, "Такой категории нет, введите заново")
+                return
         else:
             self.tg_client.send_message(msg.chat.id, "[categories list is empty]")
+            return
 
     def handle_verified_user(self, msg: Message, tg_user: TgUser):
         if not msg.text:
